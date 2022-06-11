26931be4639332e24979e09e8d6ea3def5f2ce05
Hassan Mahmud <hmahmud42@gmail.com>
2022-06-11 14:38:39 +0100

    feature/add-license: add license

--- blockchain_proto/blockchain_ds.py
+++ blockchain_proto/blockchain_ds.py
@@ -1,5 +1,12 @@
 """
-Implements a simple blockchain data structure.
+Copyright 2022, M. M. Hassan Mahmud
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+
+This file implements a simple blockchain data structure.
 """
 
 from hashlib import sha256
